a7a8c4df0eacbd1a203af3d95625a081d930f650
yeasinrafee <mdyeasinrafi7@gmail.com>
2026-01-03 00:01:39 +0600

    Intra modular dependency

--- src/tweet/tweet.controller.ts
+++ src/tweet/tweet.controller.ts
@@ -1,5 +1,8 @@
 import { Controller } from '@nestjs/common';
+import { TweetService } from './tweet.service';
 
 //http://localhost:3000/tweet
 @Controller('tweet')
-export class TweetController {}
+export class TweetController {
+  constructor(private tweetService: TweetService) {}
+}
